0300880a087a20422ce8f69191fc7de98f81da6d
quitam <tamphamasd@gmail.com>
2022-11-08 16:57:20 +0700

    first commit

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,43 @@
+import './Login.scss';
+
+import { Link } from 'react-router-dom';
+import Leaf from '../../img/login/leaf-logo1.png';
+import { Google, GitHub } from '@mui/icons-material/';
+
+const Login = () => {
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left"></div>
+                <div className="right">
+                    <img src={Leaf} alt="" />
+                    <h1 className="title">Leaf</h1>
+                    <p className="slowgan">Gone with the wind</p>
+                    <form action="">
+                        <input type="text" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button>Login</button>
+                    </form>
+                    <p className="align-left">Forgot password?</p>
+
+                    <p className="align-left">
+                        Don't have account?{' '}
+                        <span>
+                            <Link to="/register" className="register-now">
+                                Register now
+                            </Link>
+                        </span>
+                    </p>
+                    <hr />
+                    <p className="others">
+                        Login with:
+                        <Google titleAccess="Login with Google" className="icon google" />
+                        <GitHub titleAccess="Login with Github" className="icon github" />
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Register.scss';
+
+import { Link } from 'react-router-dom';
+import Leaf from '../../img/login/leaf-logo2.png';
+
+const Register = () => {
+    return (
+        <div className="register">
+            <div className="card">
+                <div className="left">
+                    <img src={Leaf} alt="" />
+                    <h1 className="title">Leaf</h1>
+                    <p className="slowgan">Gone with the wind</p>
+                    <form action="">
+                        <input type="email" placeholder="Email" />
+                        <input type="text" placeholder="Fullname" />
+                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Confirm Password" />
+
+                        <button>Login</button>
+                    </form>
+
+                    <p className="align-left">
+                        If you have account?{' '}
+                        <span>
+                            <Link to="/login" className="register-now">
+                                Login now
+                            </Link>
+                        </span>
+                    </p>
+                </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
