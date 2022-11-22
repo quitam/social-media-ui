5d0af68b99f8a24da688db9718cdb2a297957b30
quitam <tamphamasd@gmail.com>
2022-11-23 03:34:34 +0700

    dark/light header

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,8 @@ import React, { useContext, useState, useEffect } from 'react';
 import './Navbar.scss';
 
 import logo from '../../img/login/leaf-logo1.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FiSun, FiMoon, FiHome, FiSend, FiHeart, FiPlusSquare } from 'react-icons/fi';
 import { ThemeContext } from '../../GlobalComponents/ThemeProvider';
 import { Grid, Avatar } from '@mui/material';
@@ -23,19 +25,34 @@ const Navbar = () => {
             >
                 <Grid container alignItems="center" justify="center">
                     <Grid item xs={2}></Grid>
-                    <Grid item xs={3}>
+                    <Grid item xs={2}>
                         <img src={logo} alt="logo" width="50px" />
                     </Grid>
                     <Grid item xs={3}>
-                        <input type="text" placeholder="Search" />
+                        <div className={`${darkMode ? 'theme-search-dark' : ''} search`}>
+                            <input type="text" placeholder="Search accounts" spellCheck={false} />
+                            <button className="clear">
+                                <FontAwesomeIcon icon={faCircleXmark} />
+                            </button>
+                            <FontAwesomeIcon className="loading" icon={faSpinner} />
+
+                            <span></span>
+                            <button className="search-btn">
+                                <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            </button>
+                        </div>
                     </Grid>
-                    <Grid item xs={3} display="flex" alignItems="center">
+                    <Grid item xs={3} style={{ display: 'flex', justifyContent: 'end' }}>
                         <FiHome title="Home" size="30px" className="navbar__icon" />
                         <FiSend title="Messages" size="30px" className="navbar__icon" />
                         <FiHeart title="Notifications" size="30px" className="navbar__icon" />
                         <FiPlusSquare title="Create" size="30px" className="navbar__icon" />
 
-                        <div title="Dark/Light mode" style={{ height: '30px' }} onClick={() => setDarkMode(!darkMode)}>
+                        <div
+                            title="Dark/Light mode"
+                            style={{ height: '30px', marginLeft: '20px' }}
+                            onClick={() => setDarkMode(!darkMode)}
+                        >
                             {darkMode ? (
                                 <FiMoon size="30px" className="navbar__icon" />
                             ) : (
