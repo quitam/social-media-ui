3a30d7f282da56e7acc0869e8687721bcdec259d
quitam <tamphamasd@gmail.com>
2022-12-09 03:55:33 +0700

    popup

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,15 @@
 import React, { useContext, useState, useEffect } from 'react';
 import './Navbar.scss';
+import { Modal, ModalHeader, ModalBody, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Link } from 'react-router-dom';
 import logoLight from '../../assets/images/logo/logo-light.png';
 import logoDark from '../../assets/images/logo/logo-dark.png';
 import { FiSun, FiMoon, FiHome, FiSend, FiHeart, FiPlusSquare } from 'react-icons/fi';
+import { GrClose } from 'react-icons/gr';
+import { FcAddImage } from 'react-icons/fc';
+
 import { ThemeContext } from '../../GlobalComponents/ThemeProvider';
 import { Grid, Avatar } from '@mui/material';
 import Search from '../Search';
@@ -12,13 +17,90 @@ import Search from '../Search';
 const Navbar = () => {
     const { theme, setThemeMode } = useContext(ThemeContext);
     const [darkMode, setDarkMode] = useState(theme);
+    const [modal, setModal] = useState(false);
+    const [picture, setPicture] = useState();
+
     useEffect(() => {
         setThemeMode(darkMode);
         console.log(darkMode);
     }, [darkMode, setThemeMode]);
 
+    useEffect(() => {
+        return () => {
+            picture && URL.revokeObjectURL(picture.preview);
+        };
+    }, [picture]);
+    const handlePreview = (e) => {
+        const file = e.target.files[0];
+        file.preview = URL.createObjectURL(file);
+        setPicture(file);
+    };
+    console.log(picture);
     return (
         <div>
+            <Modal size="lg" centered show={modal} onHide={() => setModal(!modal)}>
+                <ModalHeader closeButton={true}>Create a new Post</ModalHeader>
+                <ModalBody>
+                    <form action="">
+                        <Row>
+                            <div className="d-flex">
+                                <Col>
+                                    <div
+                                        className={`d-flex ${
+                                            picture
+                                                ? 'justify-content-between'
+                                                : 'justify-content-center align-items-center h-100 d-inline-block'
+                                        }`}
+                                    >
+                                        <input className="d-none" type="file" onChange={handlePreview} id="file" />
+                                        <label htmlFor="file" role="button">
+                                            <FcAddImage size="50px" />
+                                            {picture ? (
+                                                <span className="ms-2">{picture.name}</span>
+                                            ) : (
+                                                <span className="ms-2">Add a picture</span>
+                                            )}
+                                        </label>
+                                        {picture && (
+                                            <button
+                                                className="bg-transparent"
+                                                type="button"
+                                                onClick={() => setPicture()}
+                                            >
+                                                <GrClose />
+                                            </button>
+                                        )}
+                                    </div>
+                                    {picture && <img src={picture.preview} alt="error" width="100%" />}
+                                    {/* <img
+                                            src="https://images.unsplash.com/photo-1611262588024-d12430b98920?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aW5zdGFncmFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                                            alt=""
+                                            style={{ width: '100%' }}
+                                        /> */}
+                                </Col>
+                                <Col>
+                                    <div className="d-flex align-items-center ms-3">
+                                        <Avatar />
+                                        <div className="ms-3">tampham_2929</div>
+                                    </div>
+                                    <div>
+                                        <textarea
+                                            rows="10"
+                                            placeholder="Write a caption..."
+                                            className="form-control mt-3 ms-1 ps-3 pt-3"
+                                        ></textarea>
+                                    </div>
+                                </Col>
+                            </div>
+                        </Row>
+                        <div className="d-flex justify-content-end">
+                            <button className="btn btn-primary mt-3" style={{ fontSize: '1.5rem' }}>
+                                Post
+                            </button>
+                        </div>
+                    </form>
+                </ModalBody>
+            </Modal>
             <div
                 className={`${darkMode ? 'theme-dark' : 'theme-light'} navbar__barContent`}
                 justifyContent="center"
@@ -31,7 +113,7 @@ const Navbar = () => {
                             <img
                                 src={darkMode ? logoDark : logoLight}
                                 alt="logo"
-                                height="54px"
+                                height="58px"
                                 style={{ paddingTop: '4px' }}
                             />
                         </Link>
@@ -44,7 +126,12 @@ const Navbar = () => {
                         <FiHome title="Home" size="30px" className="navbar__icon" />
                         <FiSend title="Messages" size="30px" className="navbar__icon" />
                         <FiHeart title="Notifications" size="30px" className="navbar__icon" />
-                        <FiPlusSquare title="Create" size="30px" className="navbar__icon" />
+                        <FiPlusSquare
+                            title="Create"
+                            size="30px"
+                            className="navbar__icon"
+                            onClick={() => setModal(!modal)}
+                        />
 
                         <div
                             title="Dark/Light mode"
@@ -57,7 +144,9 @@ const Navbar = () => {
                                 <FiSun size="30px" className="navbar__icon" />
                             )}
                         </div>
-                        <Avatar sx={{ bgcolor: 'red', width: '30px', height: '30px' }}>N</Avatar>
+                        <Link to="/profile">
+                            <Avatar sx={{ bgcolor: 'red', width: '30px', height: '30px' }}>TY</Avatar>
+                        </Link>
                     </Grid>
                     <Grid item xs={1}></Grid>
                 </Grid>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
+import { Modal, ModalHeader, ModalBody, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { useThemeHook } from '../../GlobalComponents/ThemeProvider';
 import Navbar from '../../components/Navbar';
@@ -9,21 +11,80 @@ import './Profile.scss';
 
 const Profile = () => {
     const [theme] = useThemeHook();
+    const [modal, setModal] = useState(false);
 
     useEffect(() => {
         document.title = 'Leaf | Profile';
     });
     return (
         <div>
+            <Modal centered show={modal} onHide={() => setModal(!modal)}>
+                <ModalHeader closeButton={true}>Edit Profile</ModalHeader>
+                <ModalBody>
+                    <form action="">
+                        <Row>
+                            <div className="d-flex align-items-center">
+                                <Col lg={3}>
+                                    <label>Name</label>
+                                </Col>
+                                <input type="text" className="form-control" placeholder="Enter Name" />
+                            </div>
+                            <div className="mt-5 d-flex align-items-center">
+                                <Col lg={3}>
+                                    <label>Username</label>
+                                </Col>
+                                <input type="text" className="form-control" placeholder="Enter Username" />
+                            </div>
+
+                            <div className="mt-5 d-flex align-items-center">
+                                <Col lg={3}>
+                                    <label>Bio</label>
+                                </Col>
+                                <textarea className="form-control" />
+                            </div>
+
+                            <div className="mt-5 d-flex align-items-center">
+                                <Col lg={3}>
+                                    <label>Email</label>
+                                </Col>
+                                <input type="email" className="form-control" placeholder="Enter Email" />
+                            </div>
+                            <div className="mt-5 d-flex align-items-center">
+                                <Col lg={3}>
+                                    <label>Phone Number</label>
+                                </Col>
+                                <input type="text" className="form-control" placeholder="Enter Phone" />
+                            </div>
+                            <div className="mt-5 d-flex align-items-center">
+                                <Col lg={3}>
+                                    <label>Birthday</label>
+                                </Col>
+                                <input type="text" className="form-control" placeholder="Enter Birthday" />
+                            </div>
+                            <div className="mt-5 d-flex align-items-center">
+                                <Col lg={3}>
+                                    <label>Gender</label>
+                                </Col>
+                                <input type="text" className="form-control" placeholder="Enter Gender" />
+                            </div>
+                        </Row>
+                        <div className="d-flex justify-content-end">
+                            <button className="btn btn-primary mt-3" style={{ fontSize: '1.5rem' }}>
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </ModalBody>
+            </Modal>
             <Navbar />
             <div
-                className={`${theme ? 'theme-dark' : 'bg-content-light'} container`}
-                style={{ minHeight: '2000px', paddingTop: '100px' }}
+                className={`${theme ? 'theme-dark' : 'bg-content-light'} container-profile`}
+                style={{ paddingTop: '100px' }}
             >
                 <Grid container>
                     <Grid item xs={3}></Grid>
                     <Grid item xs={2}>
-                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div style={{ display: 'flex' }}>
                             <img
                                 className="profile-image"
                                 src="https://images.unsplash.com/photo-1608889175123-8ee362201f81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
@@ -34,7 +95,12 @@ const Profile = () => {
                     <Grid item xs={4}>
                         <div className="profile">
                             <h4 className="profile-username">quitam_2929</h4>
-                            <button className={`${theme ? 'theme-dark' : ''} edit-profile`}>Edit profile</button>
+                            <button
+                                className={`${theme ? 'theme-dark' : ''} edit-profile`}
+                                onClick={() => setModal(!modal)}
+                            >
+                                Edit profile
+                            </button>
                             <button className={`${theme ? 'theme-dark' : ''} settings`}>
                                 <FiSettings size="25px" />
                             </button>
@@ -56,26 +122,40 @@ const Profile = () => {
                 </Grid>
                 <Grid container style={{ margin: '20px 0' }}>
                     <Grid item xs={2}></Grid>
-                    <Grid item xs={8} style={{ borderTop: '1px solid black', padding: '20px 0' }}>
+                    <Grid
+                        item
+                        xs={8}
+                        style={{ borderTop: theme ? '1px solid white' : '1px solid black', padding: '20px 0' }}
+                    >
                         <div className="gallery">
                             <img
                                 className="gallery-item"
-                                src="https://images.unsplash.com/photo-1608889175123-8ee362201f81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                                src="https://images.unsplash.com/photo-1601288855733-568a84692378?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNjZW5lcnl8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                                 alt=""
                             />
                             <img
                                 className="gallery-item"
-                                src="https://images.unsplash.com/photo-1608889175123-8ee362201f81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                                src="https://images.unsplash.com/photo-1565475668349-0130bea1059b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2NlbmVyeXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
                                 alt=""
                             />
                             <img
                                 className="gallery-item"
-                                src="https://images.unsplash.com/photo-1608889175123-8ee362201f81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                                src="https://images.unsplash.com/photo-1472213984618-c79aaec7fef0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2NlbmVyeXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
                                 alt=""
                             />
                             <img
                                 className="gallery-item"
-                                src="https://images.unsplash.com/photo-1608889175123-8ee362201f81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                                src="https://images.unsplash.com/photo-1610171363518-e71ef955d896?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2NlbmVyeXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60"
+                                alt=""
+                            />
+                            <img
+                                className="gallery-item"
+                                src="https://images.unsplash.com/photo-1590227521023-e362889a3adb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNjZW5lcnl8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                                alt=""
+                            />
+                            <img
+                                className="gallery-item"
+                                src="https://images.unsplash.com/photo-1564661392417-bb629e9c6519?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNjZW5lcnl8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
                                 alt=""
                             />
                         </div>
